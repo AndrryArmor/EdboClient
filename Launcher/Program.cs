08c2d47c20c3d0b150ada3fa05d36bcae89a4f96
AndrryArmor <andrry1@gmail.com>
2022-08-18 19:22:26 +0300

    Changed second priority upper limit for 126 and 123 specialities to non-passing score of the 121 speciality.

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -11,13 +11,14 @@ namespace SilentThief
             Console.WriteLine("----------------------------------------");
             var fice121Stats = await AbiturientOfferManager.GetStatsFor(Specialities.Fice121);
             AbiturientOfferManager.PrintStats(fice121Stats);
+            var limit = fice121Stats.GeneralPassingScore;
 
             var fice126Stats = await AbiturientOfferManager.GetStatsFor(Specialities.Fice126);
-            var fice126SpecialStats = await AbiturientOfferManager.GetStatsFor(Specialities.Fice126, 195);
+            var fice126SpecialStats = await AbiturientOfferManager.GetStatsFor(Specialities.Fice126, limit);
             AbiturientOfferManager.PrintStats(fice126Stats, fice126SpecialStats);
 
             var fice123Stats = await AbiturientOfferManager.GetStatsFor(Specialities.Fice123);
-            var fice123SpecialStats = await AbiturientOfferManager.GetStatsFor(Specialities.Fice123, 195);
+            var fice123SpecialStats = await AbiturientOfferManager.GetStatsFor(Specialities.Fice123, limit);
             AbiturientOfferManager.PrintStats(fice123Stats, fice123SpecialStats);
 
             Console.ReadKey();
